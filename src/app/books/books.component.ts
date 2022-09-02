3aa6cc179c891e3e977bd53a1dd46b413c47c7dc
lyndia <lyndia.libin@uberfusion.com>
2022-09-02 15:31:35 +0800

    directives - ngIf

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  isDisabled = false;
+  isShowing: boolean = true;
   name: string = 'Clean Code';
   author: string  = 'Robert C Martin';
   src: string  = 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg'
@@ -15,14 +15,5 @@ export class BooksComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  handleClick() {
-    this.isDisabled = true;
-   // alert ("I am working");
-  }
-
-  myName : string = "";
-  handleInput(event:any) {
-    this.myName = event.target.value;
-  }
 
 }
