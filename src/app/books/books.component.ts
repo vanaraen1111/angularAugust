8bef681996f994b899533979c8a055e0c0deb6ab
lyndia <lyndia.libin@uberfusion.com>
2022-09-02 15:49:37 +0800

    ngFor

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book {
+  name:string
+  author:string
+  image:string
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  isShowing: boolean = true;
-  name: string = 'Clean Code';
-  author: string  = 'Robert C Martin';
-  src: string  = 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg'
+
+  isShowing: boolean =  true;
+  books: Book[] = [{
+    name:  'Clean Code',
+    author:   'Robert C Martin',
+    image:    'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1323/9780132350884.jpg'
+  },{
+    name:  'Pragmatic Programmer',
+    author:   'David Thomas',
+    image:    'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/41HXiIojloL._AC_UL116_SR116,116_.jpg'
+  }]
+
   constructor() { }
 
   ngOnInit(): void {
